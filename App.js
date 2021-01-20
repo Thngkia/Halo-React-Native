e3146b4aa8bc8dffba918850ebfada058f836d14
Jeremy <jeremyncc94@hotmail.com>
2021-01-20 22:44:21 +0800

    RN init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+// import EmojiDict from './src/components/EmojiDict';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App page</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screens/QrCodeScreen.js
@@ -0,0 +1,123 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {
+  AppRegistry,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+import QRCode from 'react-native-qrcode-svg';
+
+class App extends Component {
+  state = {
+    qr: '',
+  };
+  onRead = (e) => {
+    this.setState({qr: e.data});
+  };
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            <QRCodeScanner
+              onRead={this.onRead}
+              flashMode={RNCamera.Constants.FlashMode.torch}
+              cameraProps={{captureAudio: false}}
+              topContent={
+                <Text>
+                  Go to <Text>wikipedia.org/wiki/QR_code</Text> on your computer
+                  and scan the QR code.
+                </Text>
+              }
+              bottomContent={
+                <TouchableOpacity style={styles.buttonTouchable}>
+                  <Text>{this.state.qr}</Text>
+                </TouchableOpacity>
+              }
+            />
+            {this.state.qr ? <QRCode value={this.state.qr} /> : null}
+
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Test</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>{this.state.qr}</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
